dafe17ba60a63bd02378c33cc6e7f2f7efb14534
Nguyễn Thư <ngthu8484@gmail.com>
2024-12-23 15:52:48 +0700

    Cập nhật file templates

---
+++ app_CapNhatHienThiBangGiaVang/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CapnhathienthibanggiavangConfig(AppConfig):
+    name = 'app_CapNhatHienThiBangGiaVang'
---
+++ app_KhuyenMaiUuDai/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class QuanlykhuyenmaiuudaiConfig(AppConfig):
+    name = 'app_KhuyenMaiUuDai'
