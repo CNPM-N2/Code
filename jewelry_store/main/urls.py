0bb5c10ae1af35291ef29e420d7d63753aee2057
YanjChris <texwall4567.1@gmail.com>
2025-02-13 04:21:11 +0700

    add

--- jewelry_store/main/urls.py
+++ jewelry_store/main/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.urls import path, include
+from main.views import product_list
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('products/', views.product_list, name='product_list'),
     path('product/<slug:slug>/', views.product_detail, name='product_detail'),
-    path('register/', views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    path('accounts/', include('allauth.urls')),
+    path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(next_page="home"), name="logout"),
 ]
