a6c1857b8a7b4eaf8190dc39e8067062403b6cf1
Trong Phuc <texwall4567.1@gmail.com>
2025-02-17 05:30:06 +0700

    Delete admin.py

--- admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import SanPham, DanhMuc, Vang, ChiTietDa
-
-admin.site.register(SanPham)
-admin.site.register(DanhMuc)
-admin.site.register(Vang)
-admin.site.register(ChiTietDa)
